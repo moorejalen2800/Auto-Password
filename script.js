519ff1d69a3354280cb3896511c209dc1c9a3c1e
Jalen Moore <100977121+moorejalen2800@users.noreply.github.com>
2022-03-17 19:10:35 -0400

    Update script.js
    fixed JavaScript by added functions to generate return

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-function generatePassword(){
-  console.log("Hey! You Clicked The Botton!")
 
 
 
-
-
-  return "Generated password will go here"
-}
 
 
  var numbers = ['0','1','2','3','4','5','6','7','8','9'];
@@ -19,12 +10,102 @@ function generatePassword(){
  var special = ['@','%','+','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'
  ];
 
-var numbers = ["0123456789"]
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUV"]
-var special = ["@ % + /  ! '# $ ^ ? : ' ) ( } { ] [ ~ - _ ."]
+function gitPasswordOption (){
+var length = parseInt(
+  prompt("How many characters would you like your password to be?")
+);
+
+if (Number.isNaN(length)) {
+  alert("Password length must be provided as a number");
+  return null;
+}
+if (length < 8) {
+  alert("password must be more than 8 characters");
+}
+if (length > 120) {
+  alert("password must be less than 120 characters");
+}
+
+var hasNumbers = confirm("Click ok if you want numbers in your password?");
+var hasUppercase = confirm("Click ok if you want uppercase in your password?");
+var hasLowercase = confirm("Click ok if you want lowercase in your password?");
+var hasSpecial = confirm("Click ok if you want special characters in your password?");
+
+if (
+  hasNumbers === false &&
+  hasUppercase === false &&
+  hasLowercase === false &&
+  hasSpecial === false
+) {
+  alert("Must select at least one character type");
+  return null;
+}
+
+var passwordOptions = {
+  length: length,
+  hasNumbers: hasNumbers,
+  hasUppercase: hasUppercase,
+  hasLowercase: hasLowercase,
+  hasSpecial: hasSpecial,
+};
+return passwordOptions;
+
+}
 
 
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
+
+
+function generatePassword() {
+  var options = gitPasswordOption();
+  var result = [];
+  var possibleCharacters = [];
+  var guaranteedCharacters = [];
+
+  if (!options) return null;
+
+  if (options.hasNumbers) {
+    possibleCharacters = possibleCharacters.concat(numbers);
+    guaranteedCharacters.push(getRandom(numbers));
+  }
+  if (options.hasUppercase) {
+    possibleCharacters = possibleCharacters.concat(upperCase);
+    guaranteedCharacters.push(getRandom(upperCase));
+  }
+    if (options.hasLowercase) {
+      possibleCharacters = possibleCharacters.concat(lowerCase);
+      guaranteedCharacters.push(getRandom(lowerCase));
+    }
+      if (options.hasSpecial) {
+        possibleCharacters = possibleCharacters.concat(special);
+        guaranteedCharacters.push(getRandom(special));
+      }
+
+      for ( var i = 0; i < options.length; i++) {
+        var possibleCharacter = getRandom(possibleCharacters);
+
+        result.push(possibleCharacter);
+      }
+
+      for ( var i = 0; i < guaranteedCharacters.length; i++) {
+        result[i] = guaranteedCharacters[i];
+      }
+      return result.join('');
+}
+
+
+
+
+
+
+
+
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
